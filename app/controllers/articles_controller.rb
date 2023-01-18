3e29497880f311b0eb1e9e1fcd81296ba6552c21
mjigar <mjigar848@gmail.com>
2023-01-18 17:28:16 +0530

    Heading to styling

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :destroy, :edit, :update]
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+    
+    def edit
+    end
+
+    def show
+    end
+
+    def create
+        @article = Article.new(article_params)
+        
+        if @article.save
+            flash[:notice] = "Your Article is successfully created"
+            redirect_to article_path(@article)
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def update
+         
+        if @article.update(article_params)
+            flash[:notice] = "Your Article is successfully updated"
+            redirect_to article_path(@article)
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+    
+    def destroy
+        @article.destroy
+        flash[:notice] = "Your Article is successfully deleted"
+        redirect_to articles_path, status: :see_other
+    end
+    private
+        def set_article
+            @article = Article.find(params[:id])
+        end
+        def article_params
+            params.require(:article).permit(:title, :description)
+        end
+end
