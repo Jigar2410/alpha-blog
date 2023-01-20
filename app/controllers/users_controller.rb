d135d2523accfe9826e4500a4c020f0da6ba1eda
mjigar <mjigar848@gmail.com>
2023-01-20 14:47:14 +0530

    edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+    
     def create
         @user = User.new(user_params)
         if @user.save
@@ -13,6 +17,17 @@ class UsersController < ApplicationController
         end
     end
 
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Your account was successfully updated"
+            redirect_to articles_path
+        else 
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    
     private
         def user_params
             params.require(:user).permit(:username, :email, :password)
